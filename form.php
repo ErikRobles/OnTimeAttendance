b8a6375481dd0031e04b1903e7cb874fa030d405
Erik Robles <30667777+ErikRobles@users.noreply.github.com>
2018-02-14 14:46:11 -0600

    Add files via upload
    Changed favicon for a black one. 
    Added forgot your password page
    Modified login page for consistent style.

--- form.php
+++ form.php
@@ -3,7 +3,7 @@
 <html lang="es">
 <head>
   <meta charset="utf-8">
-  <link rel="icon" href="img/clock.ico">
+  <link rel="icon" href="img/blkclock.ico">
   <title>User Form</title>
   <link href="//db.onlinewebfonts.com/c/a4e256ed67403c6ad5d43937ed48a77b?family=Core+Sans+N+W01+35+Light" rel="stylesheet" type="text/css"/>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">    <link rel="stylesheet" href="css/bootstrap.min.css">
@@ -13,7 +13,7 @@
 </head>
 <body>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="index.html"><img src="img/clock.svg" class="clock" style="margin-right: 5px; margin-top: -15px; width: 100px;"></img><span class="bigFont">O</span>n Time Attendance</a>
+    <a class="navbar-brand" href="index.html"><img src="img/clock.svg" class="clock" style="margin-right: 5px; margin-top: ; width: 100px;"></img><span class="bigFont">O</span>n Time Attendance</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -21,7 +21,7 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto">
         <li class="nav-item active">
-          <a class="nav-link" href="#"><i class="fa fa-home" style="margin-right: 5px;"></i>Home <span class="sr-only"></span></a>
+          <a class="nav-link" href="#"><i class="fa fa-home" style="margin-right: 5px;"></i>Inicio <span class="sr-only"></span></a>
         </li>
         <li class="nav-item active">
           <a class="nav-link" href="dashboard.html"><i class="fa fa-tachometer" aria-hidden="true" style="margin-right: 5px;"></i>Dashboard <span class="sr-only">(current)</span></a>
@@ -29,10 +29,10 @@
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fa fa-sign-in" aria-hidden="true" style="margin-right: 5px;"></i>Log-In</a>
           <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="form.php"><i class="fa fa-user-plus" aria-hidden="true" style="margin-right: 5px;"></i>Register</a>
-            <a class="dropdown-item" href="#"><i class="fa fa-sign-in" aria-hidden="true" style="margin-right: 5px;"></i>Sign-In</a>
+            <a class="dropdown-item" href="form.php"><i class="fa fa-user-plus" aria-hidden="true" style="margin-right: 5px;"></i>Registrar</a>
+            <a class="dropdown-item" href="login.html"><i class="fa fa-sign-in" aria-hidden="true" style="margin-right: 5px;"></i>Log-In</a>
             <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#"><i class="fa fa-server" aria-hidden="true" style="margin-right: 5px;"></i>Products</a>
+            <a class="dropdown-item" href="#"><i class="fa fa-server" aria-hidden="true" style="margin-right: 5px;"></i>Productos</a>
           </div>
         </li>
         </ul>
